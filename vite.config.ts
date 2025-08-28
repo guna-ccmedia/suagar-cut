0ce58f758af8623ddb3e68211a17aea584668e84
guna-ccmedia <gunasekaranccmedia@gmail.com>
2025-08-28 13:59:43 +0530

    Prepare for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Changed from '/sugar-cut/' for local development
+  base: '/',  // Change this line
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
